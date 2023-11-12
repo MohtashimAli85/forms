3da42c9d2f3548e9a3e7b9ce64d4fe02f31c3132
Mohtashim Ali <mohtashima85@gmail.com>
2023-11-12 19:30:34 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/assessment/general',
+        permanent: true,
+        basePath: false
+      }
+    ];
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
