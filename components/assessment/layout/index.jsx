bb0a8b90b311e4c715c7499ea67a01dcd5d5e3d3
Mohtashim Ali <mohtashima85@gmail.com>
2023-11-16 14:45:51 +0500

    Update index.jsx

--- components/assessment/layout/index.jsx
+++ components/assessment/layout/index.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import EvaluationSteps from '../components/EvaluationTypes';
+import EvaluationSteps from '../components/EvaluationTypes'
+import LogoutButton from './LogoutButton'
 
 const AssessmentLayout = ({ children }) => {
+  
   return (
     <>
+    <div className='flex justify-between'>
+
       <h1 className='text-3xl'>Choose your evaluation form</h1>
+      <LogoutButton/>
+    </div>
       <EvaluationSteps />
 
       {children}
