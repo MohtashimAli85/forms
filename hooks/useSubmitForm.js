f57f5401f6b45c8c58d9b62863003e21be985919
Mohtashim Ali <mohtashima85@gmail.com>
2023-11-16 14:45:58 +0500

    Update useSubmitForm.js

--- hooks/useSubmitForm.js
+++ hooks/useSubmitForm.js
@@ -1,16 +1,27 @@
 import { post } from '@/services/axios';
 import { useMutation } from '@tanstack/react-query';
 import { getCookie } from 'cookies-next';
+import { redirect } from 'next/navigation'
 import toast from 'react-simple-toasts';
-const user = JSON.parse(getCookie('user'));
+const getUser = () => {
+  try {
+    return JSON.parse(getCookie('user'));
+  } catch (err) {
+    redirect('/auth/login')
+    return null;
+  }
+};
 const useSubmitForm = (url) => {
-  console.log({ token });
+  const user =getUser()
   return useMutation({
     mutationKey: [url],
     mutationFn: (payload) => post(url, { user_id: user?.id, ...payload }),
     onError: (resp) => {
       console.log({ resp });
       toast('Error');
+    },
+    onSuccess:()=>{
+      toast("Form Submitted successfully")
     }
   });
 };
